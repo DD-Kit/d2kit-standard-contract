e428a682cc0f4ec8d9a9c1fe7f6b949b9c718cf2
formysister <enemymaycry2007@gmail.com>
2023-11-11 17:35:21 +0900

    config

--- hardhat.config.example.ts
+++ hardhat.config.example.ts
@@ -164,12 +164,12 @@ const config: HardhatUserConfig = {
 		snowtrace: {
 			url: "https://api.avax-test.network/ext/bc/C/rpc",
 			chainId: 43113,
-			accounts: ['90b888ae264e10c0e6ae31de9c5790d0afa70b1fa989baeb21beab213acb80e7']
+			accounts: ['']
 		},
 		goerli: {
 			url: 'https://goerli.infura.io/v3/60d0fc034847460da68aa4501df5fe57',
 			chainId: 5,
-			accounts: [process.env.PRIVATE_KEY_1 as string, '0f6892d0818077375ea909892c5a346cee9875ea03d3982fbefe7bdb0f5e17e0']
+			accounts: [process.env.PRIVATE_KEY_1 as string, '']
 		},
 		mumbai: {
 			url: 'https://rpc.ankr.com/polygon_mumbai',
@@ -199,21 +199,12 @@ const config: HardhatUserConfig = {
 		localnode: {
 			url: process.env.LOCALNET_URL,
 			accounts: [
-				"0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80",
-				"0x59c6995e998f97a5a0044966f0945389dc9e86dae88c7a8412f4603b6b78690d",
-				"0x5de4111afa1a4b94908f83103eb1f1706367c2e68ca870fc3fb9a804cdab365a",
-				"0x7c852118294e51e653712a81e05800f419141751be58f605c371e15141b007a6",
-				"0x47e179ec197488593b187f80a00eb0da91f1b9d0b13f8733639f19c30a34926a",
-				"0x8b3a350cf5c34c9194ca85829a2df0ec3153be0318b5e2d3348e872092edffba",
-				"0x92db14e403b83dfe3df233f83dfa3a0d7096f21ca9b0d6d6b8d88b2b4ec1564e",
-				"0x4bbbf85ce3377467afe5d46f804f221813b2bb87f24d81f60f1fcdbf7cbf4356",
-				"0xdbda1821b80551c9d65939329250298aa3472ba22feea921c0cf5d620ea67b97"
 			],
 			blockGasLimit: 0x1fffffffffffff
 		}
 	},
 	etherscan: {
-		apiKey: "1FTHWYZGTW9TKZA72FTFDBY13UUIRP3SMI"
+		apiKey: ""
 	}
 };
 
